804de8f3d7a0e69b8e090f0ee39c6693433cbdfd
ramji013 <ramprm@gmail.com>
2025-07-26 20:40:29 +1000

    added logger

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='my_package',
+      version='0.1',
+      description='document portal',
+      author='Ramamoorthy Paramasivam',
+      packages=find_packages()
+      )
